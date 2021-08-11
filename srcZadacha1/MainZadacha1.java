e0f3acb611d9b98338da4a7ef7b1130c758dd3d3
IvanBurk <64144110+IvanBurk@users.noreply.github.com>
2021-08-11 17:00:57 +0300

    Add files via upload

---
+++ srcZadacha1/MainZadacha1.java
@@ -0,0 +1,8 @@
+public class MainZadacha1 {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int amount = 5_000_000;
+        int result = balance + amount;
+        System.out.println(result);
+    }
+}
